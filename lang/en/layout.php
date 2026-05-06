3c0c1285e74bb053012108d0728ceccd6350eea7
ErickM <erickmora0503@gmail.com>
2026-05-06 00:48:01 +0000

    feat: add translations and update interface

---
+++ lang/en/layout.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'home' => 'Home',
+    'dashboard' => 'Dashboard',
+    'login' => 'Login',
+    'register' => 'Register',
+    'welcome' => [
+        'title' => 'Welcome to CarLedger! manage your vehicles with ease.',
+        'description' => 'Your all-in-one solution for effortless vehicle management. Track maintenance, fuel, expenses, and more with ease.',
+    ],
+
+];
---
+++ lang/es/layout.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'home' => 'Inicio',
+    'dashboard' => 'Panel de Control',
+    'welcome' => '¡Bienvenido a Carledger!',
+
+];
